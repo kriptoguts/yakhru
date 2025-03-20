3b165c534e408516b41a515625cc1fbadc7acbee
kriptoguts <kriptoguts@gmail.com>
2025-03-21 03:57:59 +0700

    Update amnasd.sh

--- amnasd.sh
+++ amnasd.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Meminta input dari pengguna
-read -p "Masukkan nama wallet: " WALLET_NAME
 read -p "Masukkan nama hotkey: " HOTKEY_NAME
-read -p "Masukkan username SSH: " USER
 
 # Ambil IP server secara otomatis
 SERVER_IP=$(hostname -I | awk '{print $1}')  # Mengambil IP VPS lokal
@@ -11,16 +9,16 @@ SERVER_IP=$(hostname -I | awk '{print $1}')  # Mengambil IP VPS lokal
 echo "Menggunakan IP server: $SERVER_IP"
 
 # Path lokal dan remote
-LOCAL_WALLET_PATH="$HOME/.bittensor/wallets/$WALLET_NAME"
-REMOTE_WALLET_PATH="~/.bittensor/wallets/$WALLET_NAME"
+LOCAL_WALLET_PATH="$HOME/.bittensor/wallets/default"
+REMOTE_WALLET_PATH="~/.bittensor/wallets/default"
 
 # Menyalin hotkey
 echo "Mengirim hotkey ke server..."
-scp "$LOCAL_WALLET_PATH/hotkeys/$HOTKEY_NAME" "$USER@$SERVER_IP:$REMOTE_WALLET_PATH/hotkeys/$HOTKEY_NAME"
+scp "$LOCAL_WALLET_PATH/hotkeys/$HOTKEY_NAME" "root@$SERVER_IP:$REMOTE_WALLET_PATH/hotkeys/$HOTKEY_NAME"
 
 # Menyalin coldkeypub
 echo "Mengirim coldkeypub.txt ke server..."
-scp "$LOCAL_WALLET_PATH/coldkeypub.txt" "$USER@$SERVER_IP:$REMOTE_WALLET_PATH/coldkeypub.txt"
+scp "$LOCAL_WALLET_PATH/coldkeypub.txt" "root@$SERVER_IP:$REMOTE_WALLET_PATH/coldkeypub.txt"
 
 echo "Sinkronisasi selesai!"
 
